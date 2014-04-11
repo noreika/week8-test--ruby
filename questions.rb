6b2b30e36cc27678c81f046c5ba9c80e7457d1ca
KarolisNo <kano@kano.lt>
2014-04-11 10:19:40 +0100

    1st question

--- questions.rb
+++ questions.rb
@@ -1,5 +1,6 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+	array.select { |word| word if word[0,1] == 'a' }
 end
 
 # keep only the elements that start with a vowel
