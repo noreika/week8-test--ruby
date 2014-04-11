5267633b647284cd3af82dfb3e5258b546ac5480
KarolisNo <kano@kano.lt>
2014-04-11 19:34:16 +0100

    14th and 15th questions

--- questions.rb
+++ questions.rb
@@ -82,10 +82,12 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+	array.sort_by { |word| word.length }.shift
 end
 
 # return the longest word in an array
 def longest_word_in_array(array)
+	array.sort_by { |word| word.length }.reverse.shift
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
