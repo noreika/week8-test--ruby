2986e826edd2a77460ff1461ae9cdaa44a80abc3
KarolisNo <kano@kano.lt>
2014-04-11 10:40:16 +0100

    2nd question

--- questions.rb
+++ questions.rb
@@ -5,6 +5,8 @@ end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+	vowels = ['a', 'e', 'i', 'o', 'u']
+  array.select { |word| word if vowels.include?(word[0,1]) }
 end
 
 # remove instances of nil (but NOT false) from an array
